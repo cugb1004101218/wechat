3fdaf241ab54c17115d45fb5f516d4f853e2f4aa
zhuzekun <763687347@qq.com>
2016-05-14 17:25:28 +0800

    update

--- wechat_api/util.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import requests
-
-def check_signature(signature, timestamp, nonce, token):
-    pwd = "".join(sorted([token, timestamp, nonce]))
-    return hashlib.sha1(pwd).hexdigest() == signature
-
-def get_access_token()
